24b5509afd0dbc162b8fbd1bf2548adca5677e2f
luvres <luvres@hotmail.com>
2016-11-24 00:57:30 -0200

    base

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sleep 10
 
 echo -e "• `date` \e[01;37mStarting HDFS - NameNode DataNodes\e[00m"
 start-dfs.sh
-sleep 10
+sleep 20
 
 echo -e "• `date` \e[01;37mStarting YARN - Resource Manager\e[00m"
 start-yarn.sh
@@ -18,12 +18,12 @@ $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 #echo "127.0.0.1       $HOSTNAME" >>/etc/hosts
 
 if [[ $1 == "bash" ]]; then
-  echo "••• `date` Shell Bash"
+  echo "• `date` Shell Bash"
   /bin/bash
 fi
 
 if [[ $1 == "bash" ]]; then
-  echo "••• `date` Shell Bash"
+  echo "• `date` Shell Bash"
   /bin/bash
 fi
 
