a9698a3c230bc683e19dd883194bc49e4d2a707c
luvres <luvres@hotmail.com>
2016-11-24 04:34:57 -0200

    base

--- start.sh
+++ start.sh
@@ -10,6 +10,10 @@ start-yarn.sh
 
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 
+echo " "
+echo -e "• `date` \e[00;33mTest Hadoop Mapreduce ..\e[00m"
+hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.3.jar pi 16 1000
+
 
 if [[ $1 == "bash" ]]; then
   echo -e "• `date` \e[01;32mShell Bash\e[00m"
