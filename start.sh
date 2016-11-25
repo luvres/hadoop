8046784b89d340c1de9fa1f6052a7c61f7f02a99
luvres <luvres@hotmail.com>
2016-11-25 18:39:57 -0200

    jupyter

--- start.sh
+++ start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-set -e
 
 echo " "
 echo -e "\e[00;37m*\e[00m `date` \e[00;37mStarting Supervidord\e[00m"
 nohup /usr/bin/supervisord -c /etc/supervisord.conf &
-sleep 10
+sleep 3
 
 echo " "
 echo -e "\e[01;37m*\e[00m `date` \e[01;37mStarting HDFS - NameNode DataNodes\e[00m"
 start-dfs.sh
-sleep 3
 
 echo " "
 echo -e "\e[01;37m*\e[00m `date` \e[01;37mStarting YARN - Resource Manager\e[00m"
 start-yarn.sh
-sleep 3
 
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 
